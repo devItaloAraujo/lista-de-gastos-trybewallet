fc4c2c8bd0a60a72d165141104b9a4d9142f5766
Italo Araujo <italojsouza@gmail.com>
2023-07-20 16:18:21 -0300

    Requisito 02 OK

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,31 @@
+import { useSelector } from 'react-redux';
+
+type RootState = {
+  user:{
+    email: string;
+    password: string;
+  }
+};
+
 function Header() {
-  return <div>Header</div>;
+  const email = useSelector((state: RootState) => state.user.email);
+  return (
+    <div>
+      <h3 data-testid="email-field">
+        Email:
+        {' '}
+        { email }
+      </h3>
+      <h3 data-testid="total-field">
+        Total:
+        {' '}
+        { 0 }
+      </h3>
+      <h3 data-testid="header-currency-field">
+        BRL
+      </h3>
+    </div>
+  );
 }
 
 export default Header;
